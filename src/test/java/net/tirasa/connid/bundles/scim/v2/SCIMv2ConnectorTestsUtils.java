4dcef1f0b137c76835c48b4f3f8b9b7226933937
Andrea Patricelli <andreapatricelli@apache.org>
2023-03-20 17:48:44 +0100

    some minor refinements

--- src/test/java/net/tirasa/connid/bundles/scim/v2/SCIMv2ConnectorTestsUtils.java
+++ src/test/java/net/tirasa/connid/bundles/scim/v2/SCIMv2ConnectorTestsUtils.java
@@ -19,6 +19,7 @@ import java.util.Map;
 import java.util.Set;
 import net.tirasa.connid.bundles.scim.common.SCIMConnectorConfiguration;
 import net.tirasa.connid.bundles.scim.common.utils.SCIMUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.identityconnectors.common.logging.Log;
 import org.identityconnectors.framework.common.objects.Attribute;
 
@@ -67,8 +68,9 @@ public class SCIMv2ConnectorTestsUtils {
 
             switch (entry.getKey()) {
                 case "auth.baseAddress":
-                    connectorConfiguration.setBaseAddress(entry.getValue());
-                    connectorConfiguration.setBaseAddress("http://localhost:" + serverPort + "/v2/");
+                    connectorConfiguration.setBaseAddress(StringUtils.isBlank(entry.getValue())
+                            ? "http://localhost:" + serverPort + "/v2/"
+                            : entry.getValue());
                     break;
                 case "auth.password":
                     connectorConfiguration.setPassword(SCIMUtils.createProtectedPassword(entry.getValue()));
