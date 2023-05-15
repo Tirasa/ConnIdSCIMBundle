3ec895785f8ff702eb81ff8b70d069ffffb07179
Andrea Patricelli <andreapatricelli@apache.org>
2023-05-15 10:58:10 +0200

    restored headers and imports

--- src/main/java/net/tirasa/connid/bundles/scim/common/dto/AbstractSCIMBaseResource.java
+++ src/main/java/net/tirasa/connid/bundles/scim/common/dto/AbstractSCIMBaseResource.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2018 ConnId (connid-dev@googlegroups.com)
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,7 +22,7 @@ import org.identityconnectors.common.logging.Log;
 
 public abstract class AbstractSCIMBaseResource<MT extends SCIMBaseMeta> implements SCIMBaseResource<MT> {
 
-    private static final long serialVersionUID = 3673404125396687366L;
+    private static final long serialVersionUIDù+ = 3673404125396687366L;
 
     protected static final Log LOG = Log.getLog(AbstractSCIMBaseResource.class);
 
