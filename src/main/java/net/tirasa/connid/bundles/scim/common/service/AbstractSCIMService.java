97d6fe43fb38009be0f132a1d8eabc74934bd9a2
Samuel Garofalo <72073457+SamuelGaro@users.noreply.github.com>
2023-12-15 17:45:32 +0100

    [SCIM-20] retry and refresh token only with Authorization bearer (#48)

--- src/main/java/net/tirasa/connid/bundles/scim/common/service/AbstractSCIMService.java
+++ src/main/java/net/tirasa/connid/bundles/scim/common/service/AbstractSCIMService.java
@@ -76,12 +76,7 @@ public abstract class AbstractSCIMService<UT extends SCIMUser<
 
     protected WebClient getWebclient(final String path, final Map<String, String> params) {
         WebClient webClient;
-        if (StringUtil.isNotBlank(config.getBearerToken())
-                || (StringUtil.isNotBlank(config.getClientId())
-                && StringUtil.isNotBlank(config.getClientSecret())
-                && StringUtil.isNotBlank(config.getAccessTokenBaseAddress())
-                && StringUtil.isNotBlank(config.getAccessTokenAccept())
-                && StringUtil.isNotBlank(config.getAccessTokenNodeId()))) {
+        if (checkBearerToken()) {
 
             webClient = WebClient.create(config.getBaseAddress()).
                     header(HttpHeaders.AUTHORIZATION, "Bearer " + getBearerToken(false));
@@ -856,19 +851,32 @@ public abstract class AbstractSCIMService<UT extends SCIMUser<
             final WebClient webClient,
             final int retry) {
 
-        Response response = action.apply(
-                webClient.replaceHeader(HttpHeaders.AUTHORIZATION, "Bearer " + config.getBearerToken()));
-        if (response.getStatus() == Status.UNAUTHORIZED.getStatusCode()) {
-            if (retry < MAX_RETRIES) {
-                LOG.ok("Refreshing bearer token after UNAUTHORIZED response, try #{0}", retry);
-                // regenerate the token and update configuration
-                getBearerToken(true);
-                return executeAndRetry(action, webClient, retry + 1);
-            }
+        if (checkBearerToken()) {
+            Response response = action.apply(
+                    webClient.replaceHeader(HttpHeaders.AUTHORIZATION, "Bearer " + config.getBearerToken()));
+            if (response.getStatus() == Status.UNAUTHORIZED.getStatusCode()) {
+                if (retry < MAX_RETRIES) {
+                    LOG.ok("Refreshing bearer token after UNAUTHORIZED response, try #{0}", retry);
+                    // regenerate the token and update configuration
+                    getBearerToken(true);
+                    return executeAndRetry(action, webClient, retry + 1);
+                }
 
-            LOG.error("Max retries {0} reached after unauthorized error", MAX_RETRIES);
+                LOG.error("Max retries {0} reached after unauthorized error", MAX_RETRIES);
+            }
+            return response;
+        } else {
+            return action.apply(webClient);
         }
-        return response;
+    }
+
+    private boolean checkBearerToken() {
+        return StringUtil.isNotBlank(config.getBearerToken())
+                || (StringUtil.isNotBlank(config.getClientId())
+                && StringUtil.isNotBlank(config.getClientSecret())
+                && StringUtil.isNotBlank(config.getAccessTokenBaseAddress())
+                && StringUtil.isNotBlank(config.getAccessTokenAccept())
+                && StringUtil.isNotBlank(config.getAccessTokenNodeId()));
     }
 
     protected abstract PagedResults<UT> deserializeUserPagedResults(String node) throws JsonProcessingException;
